8cc898f3994e40224f49269af51ce826fc40cd5c
thweetkomputer <2320092610@qq.com>
2021-10-26 00:41:15 +0800

    py int32->i32

--- cal.py
+++ cal.py
@@ -1,2 +1,2 @@
 import sys
-print(sys.argv[1] + '\tret int32 ' + str(eval(sys.argv[2].replace('/', '//'))) + '\n}')
+print(sys.argv[1] + '\tret i32 ' + str(eval(sys.argv[2].replace('/', '//'))) + '\n}')
