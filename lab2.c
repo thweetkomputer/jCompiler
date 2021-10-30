91375a116b2faffba9bc60a7678f62f83f36fc06
thweetkomputer <2320092610@qq.com>
2021-10-30 19:31:07 +0800

    a

--- lab2.c
+++ lab2.c
@@ -75,6 +75,10 @@ int main (int argc, char *argv[])
 {
   int ccc;
   while ((ccc = getchar())!=EOF){
+    if (ccc == ' ') {
+      putchar(ccc);
+      continue;
+    }
     if (is_empty(ccc))
       continue;
     putchar(ccc);
